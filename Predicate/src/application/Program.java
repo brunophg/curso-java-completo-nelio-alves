de58f14ab96cdce4f7ecf651c66f13aeeeedb3ee
Bruno <brunophgjf@gmail.com>
2026-03-05 12:57:04 -0300

    Predicate 1.0

---
+++ Predicate/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Product;
+import util.ProductPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+     public static void main() {
+
+         List<Product> list = new ArrayList<>();
+
+         list.add(new Product("Tv", 900.00));
+         list.add(new Product("Mouse", 50.00));
+         list.add(new Product("Tablet", 350.00));
+         list.add(new Product("HD Case ", 90.00));
+
+         list.removeIf(new ProductPredicate());
+
+         for (Product product : list) {
+             System.out.println(product);
+         }
+    }
+}
